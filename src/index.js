aa2b72b55d343aeb9d356a3927874b32503f2112
AnnaMarcinkevich <anna.martsynkevich2020@gmail.com>
2021-02-27 15:14:19 +0300

    feat: implementation of the function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,67 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(number) {
+    let masUnits = [
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+    ];
+    let masTeen = [
+        "eleven",
+        "twelve",
+        "thirteen",
+        "fourteen",
+        "fifteen",
+        "sixteen",
+        "seventeen",
+        "eighteen",
+        "nineteen",
+    ];
+    let masDozens = [
+        "ten",
+        "twenty",
+        "thirty",
+        "forty",
+        "fifty",
+        "sixty",
+        "seventy",
+        "eighty",
+        "ninety",
+    ];
+    if (number == 0) {
+        return "zero";
+    }
+    let cifra; //текущая цифра
+    let counter = 0; // количество просмотренных цифр
+    let rezSt = "";
+    while (number != 0) {
+        counter++;
+        cifra = number % 10; //получаем очередную цифру
+        number = Math.floor(number / 10); // откидываем цифру от числа
+        if (cifra == 0) continue; //если цифра равна 0, то переходим к новой цифре
+        switch (counter) {
+            case 2: {
+                // для чисел вида 10, 20,30,
+                if (rezSt.length == 0) rezSt = masDozens[cifra - 1];
+                else {
+                    // для 11, 12, 13
+                    if (cifra == 1) rezSt = masTeen[masUnits.indexOf(rezSt)];
+                    //для остальных
+                    else rezSt = masDozens[cifra - 1] + " " + rezSt;
+                }
+                break;
+            }
+            case 3: {
+                if (rezSt.length == 0) rezSt = " hundred";
+                else rezSt = " hundred " + rezSt;
+            }
+            default:
+                rezSt = masUnits[cifra - 1] + rezSt;
+        }
+    }
+    return rezSt;
+};
